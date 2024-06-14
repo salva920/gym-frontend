65fafa18cd3516b20b3c818d35cca0dc39a13aef
unknown <bermudezbastidass@uvm.edu.ve>
2024-06-14 07:57:48 -0400

    install jwt-decode defindo

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode'; // Importar de esta manera
 
 const AuthContext = createContext();
 

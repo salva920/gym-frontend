2d54ce3be8056e7363151cf0daa8b56388dd48a6
unknown <bermudezbastidass@uvm.edu.ve>
2024-06-28 08:25:02 -0400

    Add authcontext 2

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import jwtDecode from 'jwt-decode'; // Cambia esto para que importe correctamente
+import { decode as jwtDecode } from 'jwt-decode'; // Cambia esto para que importe correctamente
 
 const AuthContext = createContext();
 

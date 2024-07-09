74e89cc684bb400187e41c912cae65c345c4c37a
unknown <bermudezbastidass@uvm.edu.ve>
2024-07-09 18:20:53 -0400

    update auth

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import * as jwtDecode from 'jwt-decode'; // Importación correcta
+import jwtDecode from 'jwt-decode'; // Importación correcta
 
 const AuthContext = createContext();
 
